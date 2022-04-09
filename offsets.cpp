dcb8ad994b805fd7fe9fdc9f1198013e335fa0ff
Isaac <isaacdelalama1013@gmail.com>
2022-04-08 21:55:27 -0500

    update offsets

--- offsets.cpp
+++ offsets.cpp
@@ -2,20 +2,20 @@
 
 // BHOP
 DWORD m_fFlags = 0x104;
-DWORD dwForceJump = 0x5248A34;
+DWORD dwForceJump = 0x527C38C;
 DWORD m_vecVelocity = 0x114;
 
 // TRIGGETBOT
-DWORD dwForceAttack = 0x31D0034;
+DWORD dwForceAttack = 0x3202970;
 DWORD m_iHealth = 0x100;
-DWORD m_iCrosshairId = 0xB3E4;
-DWORD dwEntityList = 0x4D9EAD4;
+DWORD m_iCrosshairId = 0x11838;
+DWORD dwEntityList = 0x4DD245C;
 DWORD m_iTeamNum = 0xF4;
 
 // WALLHACK
-DWORD m_iGlowIndex = 0xA438;
+DWORD m_iGlowIndex = 0x10488;
 DWORD dwGlowObjectManager = 0x52E70D0;
-DWORD m_bIsDefusing = 0x3930;
+DWORD m_bIsDefusing = 0x997C;
 
 // RADAR
 DWORD m_bSpotted = 0x93D;
